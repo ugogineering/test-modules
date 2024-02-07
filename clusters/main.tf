09cd82e7333d740f5b4d8e800352cf27104159e2
ugogineering <ugogineering@gmail.com>
2024-02-07 16:05:16 +0100

    Created node-pool module

--- clusters/main.tf
+++ clusters/main.tf
@@ -6,16 +6,3 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 1
   deletion_protection = false
 }
-
-resource "google_container_node_pool" "primary_nodes" {
-  name = "my-node-pool"
-  location = var.zone
-  project = var.project_id
-  cluster = google_container_cluster.primary.name 
-  node_count = 1
-
-  node_config {
-    preemptible = false
-    machine_type = "e2-medium"
-  }
-}
