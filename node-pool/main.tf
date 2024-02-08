90af2788770646287d4bb13f20eb8fc401c66780
ugogineering <ugogineering@gmail.com>
2024-02-08 18:56:57 +0100

    Updated the node-pool main.tf file.

--- node-pool/main.tf
+++ node-pool/main.tf
@@ -2,7 +2,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name = "my-node-pool"
   location = var.zone
   #project = var.project_id
-  cluster = var.cluster_name  #google_container_cluster.primary.name 
+  cluster = var.cluster_name   
   node_count = 1
 
   node_config {
