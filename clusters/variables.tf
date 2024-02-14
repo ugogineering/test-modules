fdc6c97db60e2d05aa2de8ced27ad8a2383df31f
ugogineering <ugogineering@gmail.com>
2024-02-14 17:47:41 +0100

    Modified node-pool module definition.

--- clusters/variables.tf
+++ clusters/variables.tf
@@ -1,15 +1,13 @@
 variable "zone" {
   description = "Default Zone"
   type = string
-  default = "us-east1-b"
 }
 variable "project_id" {
   description = "Project ID"
   type = string 
-  default = "myterraformpractice"
+  default = ""
 }
 variable "cluster_name" {
   description = "Project ID"
   type = string 
-  default = "my-gke-cluster"
 }
