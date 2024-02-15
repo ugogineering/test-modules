979a926917c34579b1d8df253d8ebe6748abcbab
ugogineering <ugogineering@gmail.com>
2024-02-15 11:39:00 +0100

    Added default values to variables in node-pool module and set them to default to empty string.

--- node-pool/variables.tf
+++ node-pool/variables.tf
@@ -1,12 +1,15 @@
 variable "zone" {
   description = "Default Zone"
   type = string
+  default = ""
 }
 variable "project_id" {
   description = "Project ID"
   type = string
+  default = ""
 }
 variable "cluster_name" {
   description = "Cluster name"
-  type = string 
+  type = string
+  default = "" 
 }
